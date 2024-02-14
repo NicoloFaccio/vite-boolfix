69d5f79148feb6b59d02a8a66bfec61cfbe45676
NicoloFaccio <nicolo.faccio1@gmail.com>
2024-02-14 11:55:18 +0100

    Created the structure for the body

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,30 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+<script>
+  import AppHeader from './components/AppHeader.vue';
+  import AppMain from './components/AppMain.vue';
+  import axios from 'axios';
+  import {store} from './store';
+
+  export default{
+    components: {
+      AppHeader,
+      AppMain
+    },
+    data(){
+      return {store}
+    },
+    methods: {
+      
+    }
+  }
+
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+  <AppHeader/>
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  <AppMain/>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-}
+<style lang="scss">
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
 </style>
---
+++ src/components/AppMain.vue
@@ -0,0 +1,18 @@
+<script>
+  import SingleCard from './SingleCard.vue';
+
+  export default{
+    name: 'AppMain',
+    components: {
+        SingleCard
+    }
+  }
+</script>
+
+<template>
+
+</template>
+
+<style scoped lang="scss">
+
+</style>
